015e94ea7dd62aa6d1755e53a286dc1ce5cc9fd0
fatikh07 <fatihuddin5@gmail.com>
2020-10-12 10:02:58 +0700

    Tutorial 2

---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+// // Class Component
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h3>Navbar</h3>
+//       </div>
+//     );
+//   }
+// }
+
+// // Functional Component
+// export default function Navbar() {
+//   return (
+//     <div>
+//       <h3>Navbar</h3>
+//     </div>
+//   );
+// }
+
+// Arrow Function Component
+const Navbar = () => {
+  return (
+    <div>
+      <h3>Navbar</h3>
+    </div>
+  );
+};
+
+export default Navbar;
